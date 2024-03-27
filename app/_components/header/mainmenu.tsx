ed8f39fe7c2d666def674fad13d35d5a2ec02b58
carlitorweb <crsanchez8646@gmail.com>
2024-03-26 21:05:02 -0400

    bug fix

--- app/_components/header/mainmenu.tsx
+++ app/_components/header/mainmenu.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function MainMenu() {
+  const pathname = usePathname();
   const isActiveLink = (currentPath: string) => {
-    const pathname = usePathname();
     let defaultStyle =
       'inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 dark:text-gray-300 hover:border-gray-300 hover:text-gray-700 dark:hover:text-gray-950';
     if (pathname === currentPath) {
